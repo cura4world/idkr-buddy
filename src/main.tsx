50fa2e9c8ddf792faeed700d260d9a1024cfc429
cura4world <cura4world@gmail.com>
2026-07-14 21:45:22 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { applyFontScale } from "./lib/fontScale";
+
+// 저장된 글자 크기 배율을 앱 시작 시 적용
+applyFontScale();
 
 // 앱 자동 업데이트: 서비스워커가 새 버전으로 교체되면 즉시 새로고침하고,
 // 앱이 포그라운드로 돌아올 때마다 새 버전이 있는지 확인합니다.
